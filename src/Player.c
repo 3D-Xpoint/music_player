41cb178b07ca9eab2093248f196470ed54d39ed3
3DXpoint <langhyoun@naver.com>
2022-05-19 21:19:44 +0900

    Modified Player.c about input argument

--- src/Player.c
+++ src/Player.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
 #include <string.h>
-#define TRUE 1
-#define FALSE 0
 
-
-typedef enum Input {add, del, list, next, prev, move, play, clear, quit, load, save} INPUT;
-
-INPUT command();
+void command();
 
 int main() {
-
     INPUT input;
+    int number_of_song = 0;
+    int number_of_command = 0;
 
-    input = command();
+    scanf("%d", number_of_song);
+    scanf("%d", number_of_command);
 
-    printf("\n%d\n", input);
-
+    for(int i=0; i<number_of_command; i++) {
+        command();
+    }
     return 0;
 }
 
-INPUT command() {
+void command() {
     char buffer[100];
-    printf("Enter the Command\n");
-    while(TRUE) {
-        scanf("%s", buffer);
-        if(!strcmp(buffer, "add"))        { printf("add"); return add; }
-        else if(!strcmp(buffer, "del"))   { printf("del"); return del; }
-        else if(!strcmp(buffer, "list"))  { printf("list"); return list; }
-        else if(!strcmp(buffer, "next"))  { printf("next"); return next; }
-        else if(!strcmp(buffer, "prev"))  { printf("prev"); return prev; }
-        else if(!strcmp(buffer, "move"))  { printf("move"); return move; }
-        else if(!strcmp(buffer, "play"))  { printf("play"); return play; }
-        else if(!strcmp(buffer, "clear")) { printf("clear"); return clear; }
-        else if(!strcmp(buffer, "quit"))  { printf("quit"); return quit; }
-        else if(!strcmp(buffer, "load"))  { printf("load"); return load; }
-        else if(!strcmp(buffer, "save"))  { printf("save"); return save; }
-        else { printf("Please enter the command again\n"); }
-    }
+    scanf("%s", buffer);
+    if(!strcmp(buffer, "add"))        { printf("add"); return add; } //string argument x1
+    else if(!strcmp(buffer, "del"))   { printf("del"); return del; } //string argument x1
+    else if(!strcmp(buffer, "list"))  { printf("list"); return list; }
+    else if(!strcmp(buffer, "next"))  { printf("next"); return next; }
+    else if(!strcmp(buffer, "prev"))  { printf("prev"); return prev; }
+    else if(!strcmp(buffer, "move"))  { printf("move"); return move; } //int argument x1
+    else if(!strcmp(buffer, "play"))  { printf("play"); return play; }
+    else if(!strcmp(buffer, "clear")) { printf("clear"); return clear; }
+    else if(!strcmp(buffer, "quit"))  { printf("quit"); return quit; }
+    else if(!strcmp(buffer, "load"))  { printf("load"); return load; } //string argument x1
+    else if(!strcmp(buffer, "save"))  { printf("save"); return save; } //string argument x1
+    else { ; }
 }
+
